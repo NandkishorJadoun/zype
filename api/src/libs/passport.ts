005015b2e132d4d329578df0094e4c99ea8e9d9a
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2026-03-23 20:52:47 +0530

    feat: add passport middlewares

---
+++ api/src/libs/passport.ts
@@ -0,0 +1,77 @@
+import passport from "passport";
+import passportJWT from "passport-jwt"
+import passportLocal from "passport-local";
+import { prisma } from "./prisma"
+import { env } from "../schemas/env.schema";
+import bcrypt from "bcryptjs";
+
+const ExtractJWT = passportJWT.ExtractJwt;
+const JWTStrategy = passportJWT.Strategy;
+const LocalStrategy = passportLocal.Strategy;
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async function (email, password, cb) {
+      try {
+        const user = await prisma.user.findUnique({
+          where: { email }
+        });
+
+        if (!user) {
+          return cb(null, false, {
+            message: "Email is not registered."
+          });
+        }
+
+        const match = await bcrypt.compare(password, user.password)
+
+        if (!match) {
+          return cb(null, false, {
+            message: "Invalid password."
+          })
+        }
+
+        return cb(null, user, {
+          message: "Logged in successfully"
+        });
+
+      } catch (error) {
+        return cb(error);
+      }
+    }
+  )
+);
+
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: env.JWT_SECRET_KEY,
+    },
+    async (jwtPayload, cb) => {
+      try {
+        const user = await prisma.user.findUnique({
+          where: {
+            id: jwtPayload.id,
+          },
+          select: {
+            id: true,
+            email: true,
+            username: true,
+          }
+        });
+
+        return user ? cb(null, user) : cb(null, false)
+
+      } catch (error) {
+        return cb(error);
+      }
+    }
+  )
+);
+
+export { passport };

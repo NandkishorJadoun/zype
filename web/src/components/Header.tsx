ede66a52b452fad4bbd6cf32086b497388852c86
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2026-04-10 19:46:51 +0530

    feat: add Header for list pages

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,6 +1,17 @@
-const Header = () =>{
-    const username = JSON.parse(localStorage.getItem("user")) ?? "User";
+import { Link } from "react-router";
+const { username } = JSON.parse(localStorage.getItem("user"));
 
-    
-}
+export const Header = () => {
+  const profile = username[0].toUpperCase();
 
+  return (
+    <header className="flex justify-between items-center border-b py-3 px-2">
+      <Link className="text-2xl font-semibold" to={"/chats"}>
+        Zype
+      </Link>
+      <button className="border w-8 h-8 rounded-[50%] flex items-center justify-center">
+        {profile}
+      </button>
+    </header>
+  );
+};

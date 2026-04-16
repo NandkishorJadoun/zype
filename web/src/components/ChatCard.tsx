5b37e9e8815f53e41b0fd304d8cd0ae55f1f0cd8
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2026-04-16 17:28:41 +0530

    feat: add kebab menu, dropdown logic and delete chat logic

--- web/src/components/ChatCard.tsx
+++ web/src/components/ChatCard.tsx
@@ -1,11 +1,20 @@
-import { Link } from "react-router";
+import { Link, useFetcher } from "react-router";
 import type { Chat } from "../types";
 import { HugeiconsIcon } from "@hugeicons/react";
-import { User02FreeIcons } from "@hugeicons/core-free-icons";
+import { User02FreeIcons, MoreVerticalIcon } from "@hugeicons/core-free-icons";
+import { useRef } from "react";
 
 export const ChatCard = ({ chat }: { chat: Chat }) => {
   const { id } = chat;
   const user = chat.users[0];
+  const dialogRef = useRef(null);
+  const fetcher = useFetcher();
+
+  const openDialog = () => dialogRef.current.showModal();
+  const closeDialog = () => dialogRef.current.close();
+
+  const isDeleting = fetcher.formData?.get("id") === String(id);
+  if (isDeleting) return null;
 
   // TODO: add latest message in response
 
@@ -14,15 +23,97 @@ export const ChatCard = ({ chat }: { chat: Chat }) => {
   return (
     <div className="border-b dark:border-b-slate-800 py-3 flex items-center gap-4">
       <Link
-        to={`/users/${id}`}
+        to={`/users/${user.id}`}
         className="border dark:border-slate-700 w-10 h-10 rounded-full flex dark:bg-slate-900 items-center justify-center"
       >
         <HugeiconsIcon icon={User02FreeIcons} />
       </Link>
-      <Link to={`/chats/${id}`}>
-        <p className="font-semibold">{user.username}</p>
-        <p className="opacity-75">{message ?? "Some Message"}</p>
-      </Link>
+      <div className="flex-1 flex justify-between items-center">
+        <Link to={`/chats/${id}`}>
+          <p className="font-semibold">{user.username}</p>
+          <p className="opacity-75">{message ?? "Some Message"}</p>
+        </Link>
+
+        <div>
+          <button
+            className="cursor-pointer"
+            popoverTarget={`chat-card-dropdown-${id}`}
+            style={{ anchorName: `--anchor-${id}` }}
+          >
+            <HugeiconsIcon icon={MoreVerticalIcon} />
+          </button>
+
+          <div
+            id={`chat-card-dropdown-${id}`}
+            popover="auto"
+            className="border dark:text-slate-100 dark:bg-slate-800 dark:border-slate-600 rounded shadow-2xl "
+            style={{
+              position: "absolute",
+              margin: 0,
+              inset: "auto",
+              positionAnchor: `--anchor-${id}`,
+              top: "anchor(bottom)",
+              right: "anchor(right)",
+              marginRight: "8px",
+              marginTop: "8px",
+            }}
+          >
+            <div className="flex flex-col min-w-28 p-1 backdrop-blur-2xl ">
+              <Link
+                to={`/chats/${id}`}
+                className="text-left p-2 hover:bg-slate-700 rounded"
+              >
+                Open Chat
+              </Link>
+              <Link
+                to={`/users/${user.id}`}
+                className="text-left p-2 hover:bg-slate-700 rounded"
+              >
+                Profile
+              </Link>
+              <button
+                onClick={openDialog}
+                className="text-left p-2 hover:bg-slate-700 text-red-500 rounded"
+              >
+                Delete
+              </button>
+
+              <dialog
+                ref={dialogRef}
+                className="fixed inset-0 m-auto rounded-lg shadow-2xl backdrop:bg-slate-950/50 backdrop:backdrop-blur-sm dark:bg-slate-900 dark:text-slate-100 border dark:border-slate-700 "
+              >
+                <div className="p-6">
+                  <h3 className="text-xl font-bold mb-4">
+                    Are you sure you want to delete this chat?
+                  </h3>
+
+                  <p className="text-slate-500 dark:text-slate-400 mb-8 leading-relaxed">
+                    This will delete this chat permanently. You cannot undo this
+                    action.
+                  </p>
+
+                  <div className="flex flex-col sm:flex-row-reverse gap-3">
+                    <button
+                      className="flex-1 px-4 py-2.5 bg-red-600 hover:bg-red-700 font-semibold rounded-xl transition-colors active:scale-95"
+                      onClick={() => {
+                        fetcher.submit({ id }, { method: "delete" });
+                      }}
+                    >
+                      Delete Chat
+                    </button>
+                    <button
+                      onClick={closeDialog}
+                      className="flex-1 px-4 py-2.5 dark:bg-slate-800 hover:bg-slate-200 dark:hover:bg-slate-700 dark:text-slate-100 font-semibold rounded-xl transition-colors"
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+              </dialog>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

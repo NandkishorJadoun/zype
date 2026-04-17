901cae76f703b3c84e4e2f60350b6c2841fcdb96
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2026-04-17 22:15:18 +0530

    feat: implement cloudinary image upload utility

---
+++ api/src/utils/cloudinary.ts
@@ -0,0 +1,23 @@
+import { v2 as cloudinary } from "cloudinary";
+import { env } from "../schemas/env.schema";
+
+cloudinary.config({
+    cloud_name: env.CLOUDINARY_CLOUD_NAME,
+    api_key: env.CLOUDINARY_API_KEY,
+    api_secret: env.CLOUDINARY_API_SECRET,
+    secure: true,
+})
+
+export const uploadOnCloudinary = async (file: Express.Multer.File) => {
+    try {
+        const b64 = file.buffer.toString("base64");
+        const dataURI = `data:${file.mimetype};base64,${b64}`;
+        const res = await cloudinary.uploader.upload(dataURI, {
+            folder: "zype",
+            resource_type: "image"
+        })
+        return res
+    } catch (error) {
+        console.error(error)
+    }
+}

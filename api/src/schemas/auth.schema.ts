5e9a0e1ae1ca5b7c684551e43233ae9d5e2541ad
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2026-04-18 11:55:30 +0530

    feat: add zod schema for update user form

--- api/src/schemas/auth.schema.ts
+++ api/src/schemas/auth.schema.ts
@@ -4,15 +4,9 @@ export const UserFormData = z.object({
     username: z
         .string()
         .trim()
-        .regex(/^[a-zA-Z0-9]+$/, {
-            message: "Username must contain only letters and numbers"
-        })
-        .min(3, {
-            message: "Username must be at least 3 characters long",
-        })
-        .max(10, {
-            message: "Username must be at most 10 characters long",
-        })
+        .regex(/^[a-zA-Z0-9]+$/, { message: "Username must contain only letters and numbers" })
+        .min(3, { message: "Username must be at least 3 characters long" })
+        .max(10, { message: "Username must be at most 10 characters long" })
     ,
 
     email: z.email(),
@@ -21,4 +15,25 @@ export const UserFormData = z.object({
         .string()
         .trim()
         .min(8, { message: "Password must be at least 8 characters long" })
+})
+
+export const PatchFormData = z.object({
+    fullname: z
+        .string()
+        .trim()
+        .nonempty({ message: "Name field cannot be empty" })
+        .max(20, { message: "Name must be at most 20 characters long" }),
+
+    username: z
+        .string()
+        .trim()
+        .regex(/^[a-zA-Z0-9]+$/, { message: "Username must contain only letters and numbers" })
+        .min(3, { message: "Username must be at least 3 characters long" })
+        .max(10, { message: "Username must be at most 10 characters long" })
+    ,
+
+    about: z
+        .string()
+        .trim()
+        .max(50, { message: "About field must be at most 50 characters long" })
 })
--- api/src/schemas/env.schema.ts
+++ api/src/schemas/env.schema.ts
@@ -6,9 +6,9 @@ const envVariables = z.object({
     DATABASE_URL: z.string(),
     TEST_DATABASE_URL: z.string(),
     JWT_SECRET_KEY: z.string(),
+    CLOUDINARY_CLOUD_NAME: z.string(),
     CLOUDINARY_API_KEY: z.string(),
     CLOUDINARY_API_SECRET: z.string(),
-    CLOUDINARY_API_URL: z.string(),
 })
 
 export const env = envVariables.parse(process.env)
